1fc4459b4d8576d42d3a82ad583e7d6c3adf8407
jbn <jbn@abreka.com>
2015-11-11 15:35:36 -0500

    Test Util.shuffleInPlace, close #2

--- src/test/scala/com/generativists/thirdway/core/UtilSpec.scala
+++ src/test/scala/com/generativists/thirdway/core/UtilSpec.scala
@@ -1,5 +1,6 @@
 package com.generativists.thirdway.core
 
+import org.apache.commons.math3.random.MersenneTwister
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection.mutable
@@ -18,5 +19,25 @@ class UtilSpec extends FlatSpec with Matchers {
 
     copied(1)(1) = 2
     copied shouldEqual original
+  }
+
+  "shuffleInPlace" should "randomize the order of a given collection" in {
+    val rng = new MersenneTwister()
+    val symbols = List('A, 'B, 'C)
+    val counts = symbols.map { sym => sym -> Array(0, 0, 0) }.toMap
+
+    val n = 1000
+    (0 until n) foreach { _ =>
+      val items = mutable.ArrayBuffer(symbols: _*)
+      Util.shuffleInPlace(rng, items)
+      items.zipWithIndex.foreach { case (sym, i) => counts(sym)(i) += 1 }
+    }
+
+    val (lowerBound, upperBound) = (271.0, 398.0)  // 99.998 % CI
+
+    for((sym, tallies) <- counts; tally <- tallies) {
+      (tally > lowerBound) shouldEqual true  // Weird ScalaTest issues
+      (tally < upperBound) shouldEqual true
+    }
   }
 }
