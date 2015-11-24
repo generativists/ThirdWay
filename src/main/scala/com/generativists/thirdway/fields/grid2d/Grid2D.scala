0b4b764532250746ba3d46ad8d9ba599910e48be
John Bjorn Nelson <jbn@abreka.com>
2015-11-24 18:19:40 -0500

    Sketch terroidal wrapper

--- src/main/scala/com/generativists/thirdway/fields/grid2d/Grid2D.scala
+++ src/main/scala/com/generativists/thirdway/fields/grid2d/Grid2D.scala
@@ -8,12 +8,20 @@ abstract class Grid[T:Manifest] {
   def apply(x: Int, y: Int): T
   def update(x: Int, y: Int, value: T): Unit
   def isInBounds(x: Int, y: Int) = true
+
+  def tx(x: Int) = x
+  def ty(y: Int) = y
 }
 
 trait WidthHeightBounding[T] extends Grid[T]{
   override def isInBounds(x: Int, y: Int) = {
     x >= 0 && x < width && y >= 0 && y < height
   }
+}
+
+trait Toroidal[T] extends Grid[T] {
+  def apply(x: Int, y: Int): T
+  def update(x: Int, y: Int, value: T): Unit
 }
 
 class DenseGrid2D[T:Manifest](
@@ -23,7 +31,10 @@ class DenseGrid2D[T:Manifest](
 ) extends Grid[T] {
   protected val cells = Array.fill(height, width) { initialValue }
 
-  def apply(x: Int, y: Int) = cells(x)(y)
+  def apply(x: Int, y: Int) = cells(tx(x))(ty(y))
 
-  def update(x: Int, y: Int, value: T) = cells(x)(y) = value
+  def update(x: Int, y: Int, value: T) = cells(tx(x))(ty(y)) = value
 }
+
+
+
