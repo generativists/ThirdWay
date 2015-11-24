17ec82c692ac36f0da71065f78554d07686d7e89
John Bjorn Nelson <jbn@abreka.com>
2015-11-24 18:37:28 -0500

    Comment Neighbors-based constructs

--- src/main/scala/com/generativists/thirdway/fields/grid2d/Neighborhood.scala
+++ src/main/scala/com/generativists/thirdway/fields/grid2d/Neighborhood.scala
@@ -1,15 +1,21 @@
 package com.generativists.thirdway.fields.grid2d
 
 
+/** Defines a neighborhood over a 2D Grid given an origin.*/
 abstract class GridNeighbors {
+
+  /** @return the neighbors' locations as a pair of Ints
+    */
   def locations[T](x: Int, y: Int, grid: Grid[T]): Seq[(Int, Int)]
 
+  /** @return the value in the grid for each neighbor */
   def of[T](
     x: Int, y: Int, grid: Grid[T]
   ): Seq[T] = locations(x, y, grid) map {
     case (x, y) => grid(x, y)
   }
 
+  /** @return the location and value for each neighbor */
   def withLocations[T](
     x: Int, y: Int, grid: Grid[T]
   ): Seq[((Int,Int),T)] = locations(x, y, grid) map {
@@ -18,13 +24,27 @@ abstract class GridNeighbors {
 }
 
 
+/** Builds a Von Neumann Neighborhood.
+  *
+  * That is, a point is in the Von Neumann neighborhood if its Manhattan
+  * distance is less than some given threshold. If the distance threshold
+  * equals 1, then this is equivalent to {North, West, East, South}.
+  */
 class VonNeumann(val distance: Int) extends GridNeighbors {
   require(distance > 0, "Distance must be positive")
 
+  /** @note The returned sequence has a guaranteed ordering. It enumerates
+    *       locations in the coordinate style common to computer graphics.
+    *       That is, North West is (0, 0) and South East is (N, N). It
+    *       proceeds to East-ward (across rows) first, then moves one unit
+    *       South (down one column). This may not have any value in the future.
+    *       However, I think it may make drawing operations easier.
+    */
   override def locations[T](x0: Int, y0: Int, grid: Grid[T]) = {
     VonNeumann.neighborIndices(x0, y0, distance)
   }
 }
+
 
 object VonNeumann {
   def neighborIndices(x0: Int, y0: Int, distance: Int): Seq[(Int, Int)] = for(
@@ -36,6 +56,7 @@ object VonNeumann {
 }
 
 
+/** Filters out points are out-of-bounds according to the Grid.*/
 trait Boundaries extends GridNeighbors {
   abstract override def locations[T](
     x0: Int, y0: Int, grid: Grid[T]
